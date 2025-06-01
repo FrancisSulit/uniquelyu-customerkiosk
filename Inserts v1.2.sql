671b0754b5ecbd69e5929618b3e6b31817b416cc
saamluii <202301201@iacademy.edu.ph>
2025-06-02 00:02:57 +0800

    Updated Category and Stock Status
    - changed category description from clothing to its actual category
    
    - changed 'in stock'/'low stock' to available/unavailable in stock status

---
+++ Inserts v1.2.sql
@@ -0,0 +1,7 @@
+INSERT INTO `ITEMS` 
+(`ID`, `Name`, `Image`, `Category`, `Price`, `Size`, `Colour`, `Stock Status`, `Quantity`) VALUES
+(1, 'Crew Neck T-Shirt', NULL, 'Shirts', 19.99, 'M', 'Blue', 'Available', 100),
+(2, 'Slim Fit Jeans', NULL, 'Pants', 49.99, '32', 'Dark Blue', 'Available', 60),
+(3, 'Hooded Sweatshirt', NULL, 'Sweatshirts', 39.95, 'L', 'Gray', 'Low Stock', 15),
+(4, 'Windproof Stand Blouson', NULL, 'Jackets', 59.50, 'M', 'Black', 'Available', 30),
+(5, 'Combination Sleeveless Dress', NULL, 'Dress', 29.99, 'S', 'Red', 'Unavailable', 0);
