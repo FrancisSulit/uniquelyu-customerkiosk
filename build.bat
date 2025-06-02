c5fff60ed8738dc821d1af1d014748f861548870
erickmav <202301175@iacademy.edu.ph>
2025-06-02 22:39:59 +0800

    Added Batch File
    * Added build.bat
    * Added build.sh (for Linux/Unix)
    * Added notes file for login
    
    Dito patatakbuhin ang project

---
+++ build.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+REM Paths
+set SRC_DIR=src
+set BIN_DIR=bin
+
+set LIB_CP=
+for %%j in (lib\*.jar) do (
+    set LIB_CP=!LIB_CP!;%%j
+)
+
+set MAIN_CLASS=frontend.CustomerMain
+
+echo Cleaning previous build...
+if exist %BIN_DIR% (
+    rmdir /s /q %BIN_DIR%
+)
+mkdir %BIN_DIR%
+
+echo Compiling Java files...
+javac -cp "%LIB_CP%" -d %BIN_DIR% %SRC_DIR%\backend\*.java %SRC_DIR%\frontend\*.java %SRC_DIR%\utils\*.java
+if errorlevel 1 (
+    echo Compilation failed.
+    goto end
+)
+
+echo Compilation complete.
+
+echo Running %MAIN_CLASS%...
+echo:
+java -cp "%BIN_DIR%;%LIB_CP%" %MAIN_CLASS%
+if errorlevel 1 (
+    echo Program exited with errors.
+)
+
+:end
+echo.
+echo Press any key to exit...
+pause > nul
