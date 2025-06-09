3fb7648e412af36680712e1e719e474344534396
erickmav <202301175@iacademy.edu.ph>
2025-06-09 18:20:48 +0800

    Deleted All Files
    Deleted all files to add all updated files

--- SQL/Inserts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `ITEMS` 
-(`ID`, `Name`, `Image Location`, `Category`, `Gender`, `Price`, `Size`, `Colour`, `Stock Status`, `Quantity`) VALUES
-(1, 'Crew Neck T-Shirt', NULL, 'Clothing',  'M', 19.99, 'L', 'Blue', 'In Stock', 100),
-(2, 'Slim Fit Jeans', NULL, 'Clothing', 'F', 49.99, '32', 'Dark Blue', 'In Stock', 60),
-(3, 'Hooded Sweatshirt', NULL, 'Clothing', 'F', 39.95, 'L', 'Gray', 'Low Stock', 15),
-(4, 'Windproof Stand Blouson', NULL, 'Clothing', 'M', 59.50, 'M', 'Black', 'In Stock', 30),
-(5, 'Combination Sleeveless Dress', NULL, 'Clothing', 'F', 29.99, 'S', 'Red', 'Out of Stock', 0);
