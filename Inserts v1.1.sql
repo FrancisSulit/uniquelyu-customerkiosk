8a8d57470dc35da9eaab10a6638fb6d743b31c1c
CiabattaCereals <120253401+CiabattaCereals@users.noreply.github.com>
2025-06-09 09:44:05 +0800

    Delete Inserts v1.1.sql

--- Inserts v1.1.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `ITEMS` 
-(`ID`, `Name`, `Image`, `Category`, `Price`, `Size`, `Colour`, `Stock Status`, `Quantity`) VALUES
-(1, 'Crew Neck T-Shirt', NULL, 'Clothing', 19.99, 'M', 'Blue', 'In Stock', 100),
-(2, 'Slim Fit Jeans', NULL, 'Clothing', 49.99, '32', 'Dark Blue', 'In Stock', 60),
-(3, 'Hooded Sweatshirt', NULL, 'Clothing', 39.95, 'L', 'Gray', 'Low Stock', 15),
-(4, 'Windproof Stand Blouson', NULL, 'Clothing', 59.50, 'M', 'Black', 'In Stock', 30),
-(5, 'Combination Sleeveless Dress', NULL, 'Clothing', 29.99, 'S', 'Red', 'Out of Stock', 0);
