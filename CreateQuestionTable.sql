266bfbd6f90619aa302189b70a4164d81cd418f9
CiabattaCereals <120253401+CiabattaCereals@users.noreply.github.com>
2025-06-09 09:48:33 +0800

    Create CreateQuestionTable.sql
    From Abby's code in drive

---
+++ CreateQuestionTable.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS `question1`;
+
+CREATE TABLE `question1` (
+  `satisfied` int DEFAULT NULL,
+  `unsatisfied` int DEFAULT NULL,
+  `neutral` int DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS `question2`;
+
+CREATE TABLE `question2` (
+  `satisfied` int DEFAULT NULL,
+  `unsatisfied` int DEFAULT NULL,
+  `neutral` int DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS `question3`;
+
+CREATE TABLE `question3` (
+  `satisfied` int DEFAULT NULL,
+  `unsatisfied` int DEFAULT NULL,
+  `neutral` int DEFAULT NULL
+);
